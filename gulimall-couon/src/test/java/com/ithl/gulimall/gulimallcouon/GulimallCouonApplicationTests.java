5aa25e938507c8baecd72470d6af9629345fbe7b
liner123 <2465652971@qq.com>
2020-07-15 16:24:54 +0800

    项目构建

---
+++ gulimall-couon/src/test/java/com/ithl/gulimall/gulimallcouon/GulimallCouonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithl.gulimall.gulimallcouon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
